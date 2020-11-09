623fea9b46f21d44c43f1c45183d89898230ef11
Antonin Messinger <antonin.messinger@gmail.com>
2020-11-09 18:52:53 +0100

    add linter

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // CONFIG
-const { env } = process
+const { env } = process;
 
 // DATABASE
-import pkg from 'sequelize'
-const { Sequelize } = pkg
+import pkg from 'sequelize';
+const { Sequelize } = pkg;
 
 const sequelize = new Sequelize(
   env.POSTGRES_DB,
@@ -17,23 +17,23 @@ const sequelize = new Sequelize(
 
 async function testDatabase() {
   try {
-    await sequelize.authenticate()
-    console.log('Connection to the database has been established successfully.')
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
   } catch (error) {
-    console.error('Unable to connect to the database:', error)
+    console.error('Unable to connect to the database:', error);
   }
 }
 
-testDatabase()
+testDatabase();
 
 // SERVER
-import express from 'express'
-const server = express()
+import express from 'express';
+const server = express();
 
 server.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 server.listen(env.SERVER_PORT, env.SERVER_HOST, () => {
-  console.log(`Server listening at http://${env.SERVER_HOST}:${env.SERVER_PORT}`)
-})
+  console.log(`Server listening at http://${env.SERVER_HOST}:${env.SERVER_PORT}`);
+});
